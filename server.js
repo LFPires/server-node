fd696f51cc221ff406f85f582ccfc5ed31d99240
LFPires <jp.pires@campus.fct.unl.pt>
2016-10-06 22:59:18 +0100

    lel

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
 var http = require("http");
+var fs = require("fs");
+var events = require('events');
+
+//create event object
+var eventEmitter = new events.EventEmitter();
+
+//create event handler
+var connectHandler = function connected()
+{
+  console.log('connection succesful');
+
+  eventEmitter.emit('data_received');
+}
+
+//bind event with handler
+eventEmitter.on('connection', connectHandler);
+
+//bind data_received event with anonymous function
+eventEmitter.on('data_received', function()
+{
+  console.log('data received succesfully');
+});
+
+//fire connection event
+eventEmitter.emit('connection');
+
+fs.readFile('input.txt' , function(err, data)
+{
+  if(err)
+    return console.error(err);
+  console.log(data.toString() + "1");
+});
+var data = fs.readFileSync("input.txt");
+console.log(data.toString() + "2");
 
 http.createServer(function(request, response)
 {
