63623a3a511bb356741449720b272352da1ad732
LFPires <jp.pires@campus.fct.unl.pt>
2016-10-06 18:20:03 +0100

    2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer(function(request, response)
 {
   response.writeHead(200, {'Content-Type':'text/plain'});
 
-  response.end('Hello World\n');
+  response.end('Helloo World\n');
 }).listen(8081);
 
 console.log('server running');
